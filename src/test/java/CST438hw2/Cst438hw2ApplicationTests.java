39e5a64fc363412d876376dac70e55c8035846de
Salasc2 <salasch2@gmail.com>
2021-05-11 20:42:49 -0700

    initial commit

---
+++ src/test/java/CST438hw2/Cst438hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package CST438hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
